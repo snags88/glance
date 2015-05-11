66ca565d85d6266f4c0a962f46617f49353bab9e
Seiji Naganuma <Snaganumas@gmail.com>
2015-05-11 17:02:02 +0900

    start styling show page

--- app/helpers/albums_helper.rb
+++ app/helpers/albums_helper.rb
@@ -3,4 +3,10 @@ module AlbumsHelper
   def chronological_photos(album)
     Photo.where(album_id: album).order(:posted_time)
   end
+
+  def show_edit_menu(album)
+    if album.owner == current_user
+      render 'albums/sidebar/edit', album: album
+    end
+  end
 end
